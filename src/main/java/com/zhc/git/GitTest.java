fc0314b55e618f6a69b0435059fe8f75ec5178c6
zhc <zhc@qq.com>
2022-09-03 14:28:33 +0800

    second commit

--- src/main/java/com/zhc/git/GitTest.java
+++ src/main/java/com/zhc/git/GitTest.java
@@ -2,6 +2,7 @@ package com.zhc.git;
 
 public class GitTest {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world1");
+        System.out.println("hello world2");
     }
 }
